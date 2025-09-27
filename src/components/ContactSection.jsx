b44d2fc8d1977ca988d1cbbcbf5b8cf9d0997792
Saikumar Fulpati <saikumar16287@gmail.com>
2025-09-27 19:00:49 +0530

    Updated project

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -123,7 +123,7 @@ export const ContactSection = () => {
                   name="name"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="Mukesh Patil..."
+                  placeholder="Sai F..."
                 />
               </div>
 
@@ -141,7 +141,7 @@ export const ContactSection = () => {
                   name="email"
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="mukesh@gmail.com"
+                  placeholder="sai@gmail.com"
                 />
               </div>
 
